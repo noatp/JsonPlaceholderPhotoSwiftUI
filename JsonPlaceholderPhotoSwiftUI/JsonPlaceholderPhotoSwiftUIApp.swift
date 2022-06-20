bdaebb7b7ca4e5f6ed47f4ff219a3639f41b0728
Toan Pham <toan.chpham@gmail.com>
2022-06-20 11:42:44 -0700

    Initial Commit

---
+++ JsonPlaceholderPhotoSwiftUI/JsonPlaceholderPhotoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonPlaceholderPhotoSwiftUIApp.swift
+//  JsonPlaceholderPhotoSwiftUI
+//
+//  Created by Toan Pham on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct JsonPlaceholderPhotoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
